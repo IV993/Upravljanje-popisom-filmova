3c7eb3cb8d38d0cd53b22a28730d4af640cb5516
IV993 <ivincek2025@gmail.com>
2025-04-26 20:31:29 +0200

    Update database.py

--- database.py
+++ database.py
@@ -4,21 +4,23 @@ import sqlite3
 
 # Klasa Movie predstavlja film sa svim potrebnim atributima
 class Movie:
-    def __init__(self, title, year, genre, watched=False):
+    def __init__(self, title, year, genre, watched=False, movie_id=None):
         """
         Konstruktor za klasu Movie
         :param title: Naziv filma
         :param year: Godina izlaska filma
         :param genre: Žanr filma
         :param watched: Status gledanosti (default: False)
+        :param movie_id: Id filma
         """
+        self.id = movie_id
         self.title = title
         self.year = year
         self.genre = genre
         self.watched = watched
 
     def __repr__(self):
-        return f"Movie(title={self.title}, year={self.year}, genre={self.genre}, watched={self.watched})"
+        return f"Movie(title={self.title}, year={self.year}, genre={self.genre}, watched={self.watched}, movie_id={self.id})"
 
 # Klasa MovieManager omogućuje rad s filmovima u bazi podataka
 class MovieManager:
@@ -33,16 +35,16 @@ class MovieManager:
         """
         Kreira tablicu za pohranu filmova ako ne postoji.
         """
-        conn = sqlite3.connect(self.db_name)
-        cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS movies (
-                          id INTEGER PRIMARY KEY AUTOINCREMENT,
-                          title TEXT,
-                          year TEXT,
-                          genre TEXT,
-                          watched INTEGER DEFAULT 0)''')
-        conn.commit()
-        conn.close()
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS movies (
+                              id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              title TEXT,
+                              year TEXT,
+                              genre TEXT,
+                              watched INTEGER DEFAULT 0)''')
+            conn.commit()
+            conn.close()
 
     def add_movie(self, movie: Movie):
         """
@@ -88,7 +90,7 @@ class MovieManager:
         cursor.execute("SELECT * FROM movies")
         movies = cursor.fetchall()
         conn.close()
-        return [Movie(title=movie[1], year=movie[2], genre=movie[3], watched=bool(movie[4])) for movie in movies]
+        return [Movie(title=movie[1], year=movie[2], genre=movie[3], watched=bool(movie[4]), movie_id=movie[0]) for movie in movies]
 
     def get_unwatched_movies(self):
         """
@@ -100,4 +102,4 @@ class MovieManager:
         cursor.execute("SELECT * FROM movies WHERE watched = 0")
         movies = cursor.fetchall()
         conn.close()
-        return [Movie(title=movie[1], year=movie[2], genre=movie[3], watched=bool(movie[4])) for movie in movies]
+        return [Movie(title=movie[1], year=movie[2], genre=movie[3], watched=bool(movie[4]), movie_id=movie[0]) for movie in movies]
